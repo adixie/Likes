06b28066389499afe681ffd74bf1c63c041fc6a5
adixie <88468466+adixie@users.noreply.github.com>
2021-09-20 13:41:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Like button for Neil
+
+var likecountNeil = 9;
+var numberoflikesNeil = document.querySelector("#numberoflikesNeil");
+
+numberoflikesNeil.innerHTML = likecountNeil;
+
+function increaseLikesNeil() {
+    likecountNeil++;
+    numberoflikesNeil.innerHTML = likecountNeil;
+}
+
+// Like button for Nichole
+
+var likecountNichole = 12;
+var numberoflikesNichole = document.querySelector("#numberoflikesNichole");
+
+numberoflikesNichole.innerHTML = likecountNichole;
+
+function increaseLikesNichole() {
+    likecountNichole++;
+    numberoflikesNichole.innerHTML = likecountNichole;
+}
+
+
+// Like button for Jim
+
+var likecountJim = 9;
+var numberoflikesJim = document.querySelector("#numberoflikesJim");
+
+numberoflikesJim.innerHTML = likecountJim;
+
+function increaseLikesJim() {
+    likecountJim++;
+    numberoflikesJim.innerHTML = likecountJim;
+}
